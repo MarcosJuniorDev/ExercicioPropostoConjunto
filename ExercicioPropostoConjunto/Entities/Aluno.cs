359ff2f8629e38f66b6cd88c0adbebca6c327400
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-05-16 11:04:30 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioPropostoConjunto/Entities/Aluno.cs
@@ -0,0 +1,25 @@
+﻿namespace ExercicioPropostoConjunto.Entities
+{
+    internal class Aluno
+    {
+        public int ID { get; set; }
+
+        public Aluno(int iD)
+        {
+            ID = iD;
+        }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Aluno)) return false;
+
+            Aluno other = obj as Aluno;
+            return ID.Equals(other.ID);
+        }
+    }
+}
---
+++ ExercicioPropostoConjunto/Program.cs
@@ -0,0 +1,32 @@
+﻿using ExercicioPropostoConjunto.Entities;
+
+namespace ExercicioPropostoConjunto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Aluno> set = new HashSet<Aluno>();
+
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                set.Add(new Aluno(int.Parse(Console.ReadLine())));
+            }
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                set.Add(new Aluno(int.Parse(Console.ReadLine())));
+            }
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                set.Add(new Aluno(int.Parse(Console.ReadLine())));
+            }
+            Console.WriteLine($"Total students: {set.Count}");
+        }
+    }
+}
